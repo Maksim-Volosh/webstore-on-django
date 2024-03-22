e552207dfd294ddd86a9342c3dd06e2404142db9
Max <114184285+Maksim-Volosh@users.noreply.github.com>
2024-03-22 21:05:40 +0200

    crate a simple example

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        "title": "Home",
+        "content": "Главная страница",
+        "list": ['first', 'second'],
+        "dict": {"first": 1},
+        "bool": True,
+        
+    }
+    return render(request, "main/index.html", context)
+
+def about(request):
+    return HttpResponse("About page")
