5efee35a22f9a81d13daf1eceaea961d6b0b61c7
Max <114184285+Maksim-Volosh@users.noreply.github.com>
2024-06-18 17:49:47 +0300

    Added register form

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,6 +1,6 @@
 from dataclasses import field
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User 
 
 
@@ -10,3 +10,23 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+        
+        
+class CreateUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',                   
+        )
+        
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
